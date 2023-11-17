6f332c2ae5a735897760e3c6f1ff9633a5885777
ilyasse-brachmi <ilyassebra7@gmail.com>
2023-11-17 18:24:48 +0100

    feat: add tailwind and vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import router from './routes.js'
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App)
+  .use(router)
+  .mount("#app");
---
+++ resources/js/pages/home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import MainLayout from "../layouts/mainLayout.vue"
+</script>
+<template>
+<MainLayout>
+  <h1 class="text-red-700">this is a home page</h1>
+</MainLayout>
+</template>
