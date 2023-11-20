025ecf7b380cdeee1db3a99780b1fe23ae9b59d6
AhmedMoutaouif <124098518+AhmedMoutaouif@users.noreply.github.com>
2023-11-20 19:48:06 +0100

    feat:auth

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Auth::routes();
+Route::get('/home', function () {
+    return view('welcome');
+});
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
 
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
