0dafbafe968ea833fe15a7e873a0a01d47635693
rim-bjl <sasha02elif@gmail.com>
2023-11-20 15:31:23 +0100

    fix: components and sections structures

--- resources/js/layouts/mainLayout.vue
+++ resources/js/layouts/mainLayout.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Navbar from "../components/navbar.vue";
+import Footer from "../components/footer.vue";
 </script>
 <template lang="pug">
 div
   Navbar
   slot
+  Footer
 </template>
