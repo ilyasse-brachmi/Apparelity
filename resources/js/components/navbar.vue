546f78b6247aad938281ba0f0277c3d6fd21beae
ilyasse-brachmi <ilyassebra7@gmail.com>
2023-11-22 16:57:00 +0100

    fix: responsive

--- resources/js/components/navbar.vue
+++ resources/js/components/navbar.vue
@@ -5,7 +5,7 @@ header(class="flex items-center justify-between p-4 top-0 bg-gray-100 sticky sha
       label.dz-btn.dz-btn-ghost(tabindex='0' class='lg:hidden')
         svg.h-5.w-5(xmlns='http://www.w3.org/2000/svg' fill='none' viewbox='0 0 24 24' stroke='currentColor')
           path(stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h8m-8 6h16')
-      ul.dz-menu.dz-menu-lg.dz-dropdown-content.rounded-md.mt-4.p-2.shadow-md.bg-gray-100(tabindex='0' class='z-[1] w-[20rem] sm:w-[30rem] md:w-[40rem]')
+      ul.dz-menu.dz-menu-lg.dz-dropdown-content.rounded-md.mt-4.p-2.shadow-md.bg-gray-100(tabindex='0' class='z-[1] w-[12rem] sm:w-[30rem] md:w-[40rem]')
         li
           a(href="#" class="px-14 py-2.5 border-2 m-1 border-primary text-primary rounded-sm font-medium hover:bg-primary/20 hover:shadow-md duration-300") Home
         li
--- resources/js/sections/AdvantagesSection.vue
+++ resources/js/sections/AdvantagesSection.vue
@@ -5,7 +5,7 @@ import { advantages } from '../data/advantages.data';
 </script>
 <template lang="pug">
 div(class="container mx-auto p-4")
-  h1(class="text-4xl sm:text-5xl font-semibold text-center my-8") Advantages
+  h1(class="text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-semibold text-center my-8") Advantages
   div(class="flex items-center justify-center")
     div(class="grid grid-cols-1 sm:grid-cols-2 sm:gap-x-8 md:gap-x-16 p-4 md:p-8 lg:p-16")
       div(v-for="(advantage, index) in advantages" :key="index")
