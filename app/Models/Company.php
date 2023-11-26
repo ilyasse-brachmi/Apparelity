711899775bd4e5e2e1838d355de4c31a845674b7
Haaajar <elathajar@gmail.com>
2023-11-26 22:31:25 +0100

    AuthController

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasFactory,HasApiTokens;
 
     public $timestamps = true;
     protected $table = 'company';
--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,21 @@
 <?php
 
 use App\Http\Controllers\CompanyController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+//-----------------------AuthController-----------------//
+Route::get('register',[AuthController::class,'register']);
+Route::get('login',[AuthController::class,'login']);
+Route::middleware('auth:sanctum')->group(function () {
+Route::post('/logout', [AuthController::class, 'logout']);
 });
 //-----------------------Start Company Controller-----------------//
 Route::post('add-company',[CompanyController::class,'adding']);
