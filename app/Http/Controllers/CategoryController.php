dac42710380f627ef18222726027f0fc717847c0
AhmedMoutaouif <124098518+AhmedMoutaouif@users.noreply.github.com>
2023-11-28 17:33:38 +0100

    Feat:Category-Company-Material-Product Controllers

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function adding(Request $request){
+       Category::create([
+           'name'=>$request->name,
+       ]);
+        return response()->json('Added Successfully');
+    }
+    public function edit(Request $request){
+        $category=Category::findorfail($request->id);
+        $category->update([
+            'name'=>$request->name,
+        ]);
+        return response()->json('Updated Successfully');
+    }
+    public function delete(Request $request){
+        $category=Category::findorfail($request->id);
+        $category->products()->delete();
+        $category->delete();
+        return response()->json('Deleted Successfully');
+    }
+    public function getData(){
+        $category=Category::all();
+        return response()->json($category);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ReviewController;
-use App\Http\Controllers\categoryController;
+use App\Http\Controllers\MaterialController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -22,10 +24,35 @@ Route::post('/logout', [AuthController::class, 'logout']);
 Route::post('addReview',[ReviewController::class,'addReview']);
 Route::get('getReviews',[ReviewController::class,'getReviews']);
 //-----------------------Start Company Controller-----------------//
+
+//-----------------------Start Company Controller route-----------------//
 Route::post('add-company',[CompanyController::class,'adding']);
 Route::put('edit-company',[CompanyController::class,'edit']);
 Route::delete('delete-company',[CompanyController::class,'delete']);
 Route::get('get-company',[CompanyController::class,'getData']);
 //-----------------------End Company Controller-----------------//
+
 //-----------------------SearchController-----------------//
 Route::get('searchProduct',[SearchController::class,'searchProducts']);
+//-----------------------End Company Controller route-----------------//
+
+//-----------------------Start Category Controller route-----------------//
+Route::post('add-category',[CategoryController::class,'adding']);
+Route::put('edit-category',[CategoryController::class,'edit']);
+Route::delete('delete-category',[CategoryController::class,'delete']);
+Route::get('get-category',[CategoryController::class,'getData']);
+//-----------------------End Category Controller route-------------//
+
+//-----------------------Start Product Controller route-----------------//
+Route::post('add-product',[ProductController::class,'adding']);
+Route::put('edit-product',[ProductController::class,'edit']);
+Route::delete('delete-product',[ProductController::class,'delete']);
+Route::get('get-product',[ProductController::class,'getData']);
+//-----------------------End Product Controller route-------------//
+
+//-----------------------Start Material Controller route-----------------//
+Route::post('add-material',[MaterialController::class,'adding']);
+Route::put('edit-material',[MaterialController::class,'edit']);
+Route::delete('delete-material',[MaterialController::class,'delete']);
+Route::get('get-material',[MaterialController::class,'getData']);
+//-----------------------End Material Controller route-------------//
