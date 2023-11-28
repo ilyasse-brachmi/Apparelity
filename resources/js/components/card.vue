4f8443c47f9fe7ee489206133000783a71cc9adf
Rim Boujlajel <101755734+rim-bjl@users.noreply.github.com>
2023-11-28 15:56:01 +0100

    feat: card component (#19)

---
+++ resources/js/components/card.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+const props = defineProps({
+  productName: {
+    type: String,
+    required: true
+  },
+  companyName: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  imageProduct: {
+    type: String,
+    required: true
+  }
+})
+</script>
+<template lang="pug">
+.dz-card.px-8.bg-gray-100.shadow-xl.flex-col.flex.items-center
+	div(class="relative w-[16rem] h-[20rem] mt-8")
+		img(class="rounded-lg" :src="imageProduct")
+		div(class="absolute top-[-1rem] end-[-1rem] flex items-start gap-4 p-2 bg-primary rounded-full")
+			Icon(icon="carbon:map" class="text-white cursor-pointer text-3xl font-semibold") 
+	.dz-card-body.p-4
+		h2.dz-card-title {{ productName }}
+		p {{ companyName }} 
+		.dz-card-actions.justify-end
+			.flex.justify-end 
+				p.font-semibold {{ price }} $
+</template>
--- resources/js/pages/store.vue
+++ resources/js/pages/store.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+import Product from "../../images/jacket.png"
 import StoreLayout from "../layouts/storeLayout.vue";
+import Card from "../components/card.vue";
+
 </script>
 <template lang="pug">
 StoreLayout
+    Card(:imageProduct="Product" :productName="'Jacket'" :companyName="'Example Company Name Nike'" :price="'199'")
 </template>
