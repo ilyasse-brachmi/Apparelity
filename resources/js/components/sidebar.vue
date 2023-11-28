2703dd416bdc7d7b043a075a7e6f75ea7f5167cd
rim-bjl <sasha02elif@gmail.com>
2023-11-28 17:07:25 +0100

    fix: store layout

--- resources/js/components/sidebar.vue
+++ resources/js/components/sidebar.vue
@@ -4,8 +4,8 @@ const materials = {1:'Cotton', 2:'Leather', 3:'Linen', 4:'Nylon'};
 defineProps(['categories','price', 'materials'])
 </script>
 <template lang="pug">
-aside(class="overflow-y-auto h-screen duration-200 ltr:left-0 rtl:right-0 shadow-xl bg-white")
-	div(class="text-gray-600 min-w-[20rem] h-full w-fit duration-200")
+div(class="flex items-center bg-white h-screen overflow-hidden")
+	div(class="max-h-screen duration-200 ltr:left-0 rtl:right-0 shadow-xl overflow-y-auto h-screen text-gray-600 min-w-[20rem] w-fit duration-200")
 		div.flex.items-enter.justify-between.p-4.border-b-2
 			p.text-xl.font-semibold Filter 
 			p(class="text-primary/70 mx-6 cursor-pointer text-lg") Clear All
@@ -21,4 +21,6 @@ aside(class="overflow-y-auto h-screen duration-200 ltr:left-0 rtl:right-0 shadow
 			p(class="text-base sm:text-lg text-primary font-semibold") Materials
 			ul(class="mt-2 ml-2")
 				li(v-for="material in materials" class="hover:text-primary/70") {{ material }} 
+	div.max-h-screen.h-full.w-full.overflow-y-auto
+		slot
 </template>
