12c66c62236a01bc6280e2eca5c2e20d48679ffc
ilyasse-brachmi <ilyassebra7@gmail.com>
2023-11-29 20:32:42 +0100

    feat: auth cookies

--- resources/js/stores/auth.store.ts
+++ resources/js/stores/auth.store.ts
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import Cookies from 'js-cookie';
 
 declare interface User {
   name: string,
@@ -11,19 +12,24 @@ export const useAuth = defineStore('Auth', {
   state: () => {
     return {
       user: ref({} as User),
-      token: ref(localStorage.getItem('token') || ''),
-      isAuth: ref(false)
+      token: ref(Cookies.get('token') || ''),
+      isAuth: ref(!!Cookies.get('token'))
     }
   },
   actions: {
     setToken(newToken: string) {
-      this.token = newToken
-      localStorage.setItem('token', this.token)
+      this.token = newToken;
+      Cookies.set('token', newToken, { expires: 1 })
     },
     fetchUser(token: string, newUser: User) {
       this.setToken(token)
       this.user = newUser
       this.isAuth = true
+    },
+    logout(){
+      this.user = {} as User
+      Cookies.remove('token')
+      this.isAuth = false
     }
   }
 })
