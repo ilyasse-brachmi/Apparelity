f9eeb6a45241af5c8eeb70c7cc8d65723e4eb552
Haaajar <134287469+Haaajar@users.noreply.github.com>
2023-12-07 15:13:46 +0100

    list States and countries (#29)

---
+++ app/Http/Controllers/LocationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LocationController extends Controller
+{
+    public function countries(){
+        $countries = getCountriesNames();
+        return response()->json($countries);
+    }
+    public function states(Request $request){
+        $codeIso=$request->query('codeIso');
+        $states=getStates($codeIso);
+        return response()->json($states);
+    }
+}
