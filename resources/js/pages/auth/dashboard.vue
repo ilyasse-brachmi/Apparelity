d684d70cb8ccebeecb63cf282ae30c4acdcaa207
rim-bjl <sasha02elif@gmail.com>
2023-12-10 02:15:41 +0100

    feat: input image

--- resources/js/pages/auth/dashboard.vue
+++ resources/js/pages/auth/dashboard.vue
@@ -4,20 +4,17 @@ import Input from '@/components/AppInput.vue'
 import { $AppAxios } from "@/utils/axiosSingleton"
 import { useAuth } from "@/stores/auth.store"
 import { useRouter } from "vue-router"
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useField, useForm } from 'vee-validate'
 import * as yup from 'yup'
 import Swal from "sweetalert2"
 
-var fileSelected: { value: File; } ;
-const onFileSelected = ((e: Event & { target: HTMLInputElement })=>{
-  fileSelected.value = e.target.files[0];
-})
-
 const scheme = computed(() => {
   return yup.object({
     name: yup.string().required('Name is a required field'),
-      })
+    price: yup.string().required('Price is a required field'),
+    description: yup.string().required('Description is a required field'),
+  })
 })
 
 const store = useAuth()
@@ -32,9 +29,29 @@ const {
     value: name
 } = useField<string>('name')
 
+const {
+  handleChange: priceError,
+    value: price
+} = useField<string>('price')
+
+const {
+  handleChange: descriptionError,
+    value: description
+} = useField<string>('description')
+
+const selectedFile= ref()
+const onFileSelected = ((event: UIEvent & { target: HTMLInputElement & { files: Array<string> } })=>{
+  selectedFile.value = event.target.files[0];
+})
+
 const addProductSubmit = handleSubmit(async () => {
+  const imageFile = new FormData();
+  imageFile.append('image', selectedFile.value)
   const data = { 
     name: name.value,
+    price: price.value,
+    description: description.value,
+    image: imageFile
    }
    await $AppAxios.post('/api/product', data)
    .then(async (response) => {
@@ -76,15 +93,13 @@ DashLayout
 					div(class="mt-8")
 						Input(:labelName="'Product Name'" name="name" :type="'text'" :color="'#1d6795'" @input="nameError" :inputError="errors.name")
 					div(class="mt-8")
-						Input(:labelName="'Product Description'" name="description" :type="'text'" :color="'#1d6795'")
+						Input(:labelName="'Product Description'" name="description" :type="'text'" :color="'#1d6795'" @input="descriptionError" :inputError="errors.description")
 					div(class="mt-8")
 						Input(:labelName="'Category'" name="category" :type="'text'" :color="'#1d6795'")
 					div(class="mt-8")
-						Input(:labelName="'Price'" name="price" :type="'text'" :color="'#1d6795'" :icon="'healthicons:dollar'")
+						Input(:labelName="'Price'" name="price" :type="'text'" :color="'#1d6795'" :icon="'healthicons:dollar'" @input="priceError" :inputError="errors.price")
 					div(class="mt-8")
-						Input(:labelName="'Product Image'" name="name" :type="'text'" :color="'#1d6795'")
-					div(class="mt-8")
-						input(type="file" @change="onFileSelected")
+						Input(:labelName="'Image'" name="image" :type="'file'" :color="'#1d6795'" @change="onFileSelected")
 					div(class="mt-8")
 						Input(:labelName="'Material'" name="material" :type="'text'" :color="'#1d6795'")
 				div(class="flex justify-center mt-8")
