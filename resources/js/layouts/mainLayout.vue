a44e53702f0ab87e1c7d8b35321312d70b1c458c
Rim Boujlajel <101755734+rim-bjl@users.noreply.github.com>
2023-12-10 14:33:26 +0100

    feat: add product template (#32)

--- resources/js/layouts/mainLayout.vue
+++ resources/js/layouts/mainLayout.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import Navbar from "../components/navbar.vue";
-import Footer from "../components/footer.vue";
+import Navbar from "../components/navbar.vue"
+import Footer from "../components/footer.vue"
 </script>
 <template lang="pug">
 div
