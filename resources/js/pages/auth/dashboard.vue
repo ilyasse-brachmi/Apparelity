93eeb407eca12cd5a9b56c4bac5496c21b01b291
rim-bjl <sasha02elif@gmail.com>
2023-12-11 17:48:18 +0100

    fix: add store and display on auth pages

--- resources/js/pages/auth/dashboard.vue
+++ resources/js/pages/auth/dashboard.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import StoreLayout from '@/layouts/storeLayout.vue'
+import AppModal from "@/components/AppModal.vue"
+import Exemple from "@/pages/exemple.vue"
+import Product from "../../../images/jacket.png"
+import Card from "@/components/card.vue"
 import Input from '@/components/AppInput.vue'
 import { $AppAxios } from "@/utils/axiosSingleton"
 import { useAuth } from "@/stores/auth.store"
@@ -82,16 +86,37 @@ const addProductSubmit = handleSubmit(async () => {
     }
      })
 })
+
+const modal = ref(false)
+
+const openModal = (index: number) => {
+	modal.value = true
+}
 </script>
 <template lang="pug">
 StoreLayout
   template(v-slot:cards)
     .flex.items-center.justify-center
-      div(v-if="false")
-        p data here
+      div(v-if="true" class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 p-8")
+        div(v-for="index in 6" :key="index")
+          Card(:imageProduct="Product" :productName="'Jacket'" :companyName="'Example Company Name Nike'" :price="199" @openModal="openModal(index)")
       div(v-else class="h-screen flex flex-col items-center")
         p(class="text-xl tracking-wide font-bold") Opps... It's empty in here 
         p(class="text-base text-slate-500") No offers hase been saved yet.
+      AppModal(v-if="modal" :title="'Product Traceability'" @close="modal = false")
+        div(class="grid grid-cols-5 w-full h-full")
+          div(class="col-span-2")
+            div(class="bg-gray-100 w-full p-6 py-6")
+              div(class="min-h-[48.7rem]")
+                div(class="px-4")
+                  h1(class="text-primary font-semibold text-xl") Jacket
+                div(class="flex justify-center")
+                  div(class="w-[24rem] h-[20rem] bg-white shadow-md rounded-xl border-4 my-4 border-primar")
+                div(class="flex justify-center h-full")
+                  div(class="w-[24rem] h-[25rem] bg-white shadow-md rounded-xl border-4 my-4 border-primar")
+                //- img(:src="Product" class="w-full h-[27rem]")
+          div(class="col-span-3")
+            Exemple
   template(v-slot:inAppModal)
     div(class="flex items-center justify-center h-[90vh]")
         form(method="POST" @submit.prevent="addProductSubmit" class="flex-col items-center justify-center px-4")
--- resources/js/pages/store.vue
+++ resources/js/pages/store.vue
@@ -24,24 +24,25 @@ onMounted(async () => {
 </script>
 <template lang="pug">
 StoreLayout
-	.flex.items-center.justify-center.h-full.w-full
-		div(v-if="data.length" class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 py-8 h-full")
-			div(v-for="product in data" :key="product.original.id")
-				Card(:product="product.original" :price="199" @openModal="openModal(index)")
-		div(v-else class="w-full h-full flex items-start pt-[10rem] justify-center")
-			h1(class="text-2xl font-semibold text-gray-500") Oopss... No Product Founded
-
-	AppModal(v-if="modal" :title="'Product Traceability'" @close="modal = false")
-		div(class="grid grid-cols-5 w-full h-full")
-			div(class="col-span-2")
-				div(class="bg-gray-100 w-full p-6 py-6")
-					div(class="min-h-[48.7rem]")
-						div(class="px-4")
-							h1(class="text-primary font-semibold text-xl") Jacket
-						div(class="flex justify-center")
-							div(class="w-[24rem] h-[20rem] bg-white shadow-md rounded-xl border-4 my-4 border-primar")
-						div(class="flex justify-center h-full")
-							div(class="w-[24rem] h-[25rem] bg-white shadow-md rounded-xl border-4 my-4 border-primar")
-			div(class="col-span-3")
-				Exemple
-</template>
+	template(v-slot:cards)
+		.flex.items-center.justify-center.h-full.w-full
+			div(v-if="data.length" class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 py-8 h-full")
+				div(v-for="product in data" :key="product.original.id")
+					Card(:product="product.original" :price="199" @openModal="openModal(index)")
+			div(v-else class="w-full h-full flex flex-col items-start pt-[10rem] justify-center")
+				p(class="text-2xl tracking-wide font-semibold") Opps... It's empty in here 
+				p(class="text-base text-slate-500") No offers hase been saved yet.
+		AppModal(v-if="modal" :title="'Product Traceability'" @close="modal = false")
+			div(class="grid grid-cols-5 w-full h-full")
+				div(class="col-span-2")
+					div(class="bg-gray-100 w-full p-6 py-6")
+						div(class="min-h-[48.7rem]")
+							div(class="px-4")
+								h1(class="text-primary font-semibold text-xl") Jacket
+							div(class="flex justify-center")
+								div(class="w-[24rem] h-[20rem] bg-white shadow-md rounded-xl border-4 my-4 border-primar")
+							div(class="flex justify-center h-full")
+								div(class="w-[24rem] h-[25rem] bg-white shadow-md rounded-xl border-4 my-4 border-primar")
+				div(class="col-span-3")
+					Exemple
+	</template>
