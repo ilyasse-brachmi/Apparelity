fbf6ec5b1b22ca15f6b5d5a09a176231dc559492
Rim Boujlajel <101755734+rim-bjl@users.noreply.github.com>
2023-12-11 19:08:37 +0100

    fix: customize layout and add product to app modal (#34)

--- resources/js/pages/auth/dashboard.vue
+++ resources/js/pages/auth/dashboard.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import DashLayout from '@/layouts/dashLayout.vue'
+import StoreLayout from '@/layouts/storeLayout.vue'
+import AppModal from "@/components/AppModal.vue"
+import Exemple from "@/pages/exemple.vue"
+import Product from "../../../images/jacket.png"
+import Card from "@/components/card.vue"
 import Input from '@/components/AppInput.vue'
 import { $AppAxios } from "@/utils/axiosSingleton"
 import { useAuth } from "@/stores/auth.store"
@@ -82,26 +86,53 @@ const addProductSubmit = handleSubmit(async () => {
     }
      })
 })
+
+const modal = ref(false)
+
+const openModal = (index: number) => {
+	modal.value = true
+}
 </script>
 <template lang="pug">
-DashLayout
-	div(class="flex items-center justify-center h-[90vh]")
-		div
-			h1(class="text-center text-2xl sm:text-3xl font-semibold text-primary my-4") Add New Product
-			form(method="POST" @submit.prevent="addProductSubmit" class="flex-col items-center justify-center px-4")
-				div(class="grid grid-cols-2 gap-4")
-					div(class="mt-8")
-						Input(:labelName="'Product Name'" name="name" :type="'text'" :color="'#1d6795'" @input="nameError" :inputError="errors.name")
-					div(class="mt-8")
-						Input(:labelName="'Product Description'" name="description" :type="'text'" :color="'#1d6795'" @input="descriptionError" :inputError="errors.description")
-					div(class="mt-8")
-						Input(:labelName="'Category'" name="category" :type="'text'" :color="'#1d6795'")
-					div(class="mt-8")
-						Input(:labelName="'Price'" name="price" :type="'text'" :color="'#1d6795'" :icon="'healthicons:dollar'" @input="priceError" :inputError="errors.price")
-					div(class="mt-8")
-						Input(:labelName="'Image'" name="image" :type="'file'" :color="'#1d6795'" @change="onFileSelected")
-					div(class="mt-8")
-						Input(:labelName="'Material'" name="material" :type="'text'" :color="'#1d6795'")
-				div(class="flex justify-center mt-8")
-					button(type="submit" class="border font-bold text-2xl text-white border-white rounded-full px-16 sm:px-48 py-4 bg-primary hover:shadow-md duration-300") Add
+StoreLayout
+  template(v-slot:cards)
+    .flex.items-center.justify-center
+      div(v-if="false" class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 p-8")
+        div(v-for="index in 6" :key="index")
+          Card(:imageProduct="Product" :productName="'Jacket'" :companyName="'Example Company Name Nike'" @openModal="openModal(index)")
+      div(v-else class="h-screen flex flex-col items-center pt-[10rem]")
+        p(class="text-xl tracking-wide font-bold") Opps... It's empty in here 
+        p(class="text-base text-slate-500") No offers hase been saved yet.
+      AppModal(v-if="modal" :title="'Product Traceability'" @close="modal = false")
+        div(class="grid grid-cols-5 w-full h-full")
+          div(class="col-span-2")
+            div(class="bg-gray-100 w-full p-6 py-6")
+              div(class="min-h-[48.7rem]")
+                div(class="px-4")
+                  h1(class="text-primary font-semibold text-xl") Jacket
+                div(class="flex justify-center")
+                  div(class="w-[24rem] h-[20rem] bg-white shadow-md rounded-xl border-4 my-4 border-primar")
+                div(class="flex justify-center h-full")
+                  div(class="w-[24rem] h-[25rem] bg-white shadow-md rounded-xl border-4 my-4 border-primar")
+                //- img(:src="Product" class="w-full h-[27rem]")
+          div(class="col-span-3")
+            Exemple
+  template(v-slot:inAppModal)
+    div(class="flex items-center justify-center h-[90vh]")
+        form(method="POST" @submit.prevent="addProductSubmit" class="flex-col items-center justify-center px-4")
+          div(class="grid grid-cols-2 gap-4")
+            div(class="mt-8")
+              Input(:labelName="'Product Name'" name="name" :type="'text'" :color="'#1d6795'" @input="nameError" :inputError="errors.name")
+            div(class="mt-8")
+              Input(:labelName="'Product Description'" name="description" :type="'text'" :color="'#1d6795'" @input="descriptionError" :inputError="errors.description")
+            div(class="mt-8")
+              Input(:labelName="'Category'" name="category" :type="'text'" :color="'#1d6795'")
+            div(class="mt-8")
+              Input(:labelName="'Price'" name="price" :type="'text'" :color="'#1d6795'" :icon="'healthicons:dollar'" @input="priceError" :inputError="errors.price")
+            div(class="mt-8")
+              Input(:labelName="'Image'" name="image" :type="'file'" :color="'#1d6795'" @change="onFileSelected")
+            div(class="mt-8")
+              Input(:labelName="'Material'" name="material" :type="'text'" :color="'#1d6795'")
+          div(class="flex justify-center mt-8")
+            button(type="submit" class="border font-bold text-2xl text-white border-white rounded-full px-16 sm:px-48 py-4 bg-primary hover:shadow-md duration-300") Add
 </template>	
