4f3c93706e47d87bdd92a727b9e495e978f627ef
ilyasse-brachmi <ilyassebra7@gmail.com>
2024-11-13 16:40:22 +0100

    fix: credantials issue

--- resources/js/composables/usegeoCodingAdress.ts
+++ resources/js/composables/usegeoCodingAdress.ts
@@ -1,6 +1,6 @@
 import { $AppAxios } from "@/utils/axiosSingleton"
 import type { ReverseAdress, ForwardAdress } from "@/types/index"
-const myGeoToken = 'cd4a3b985cf04a9da8744687b983b48a' 
+const myGeoToken = import.meta.env.VITE_GEO_TOKEN
 
 export async function getReverseAdress(lat: number, lang: number): Promise<ReverseAdress> {
   return await $AppAxios.get(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lang}&key=${myGeoToken}`)
